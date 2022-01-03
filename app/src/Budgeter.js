0015dfbbf4729903ab32287ebc22e42b11690866
loezasierra <joseloeza@gmail.com>
2022-01-03 14:58:38 -0800

    Added initial state to Budgeter

--- app/src/Budgeter.js
+++ app/src/Budgeter.js
@@ -3,13 +3,24 @@ import React from 'react';
 import AmountInput from './components/AmountInput'
 import Calculator from './components/Calculator';
 
-function Budgeter() {
-  return (
-    <div>
-      <AmountInput name="paycheck" />
-      <Calculator />
-    </div>
-  );
+class Budgeter extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      paycheck: '',
+      fixedExpenses: {},
+      savings: '',
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <AmountInput name="paycheck" />
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default Budgeter;
