f0ce6ec15576d5089000296380b0b102d4a2cf9e
loezasierra <joseloeza@gmail.com>
2022-01-12 18:56:51 -0800

    Added hideable component

--- app/src/Budgeter.js
+++ app/src/Budgeter.js
@@ -2,6 +2,7 @@ import './Budgeter.css';
 import React from 'react';
 import AmountInput from './components/AmountInput'
 import Calculator from './components/Calculator';
+import Hideable from './components/Hideable';
 
 class Budgeter extends React.Component{
   constructor(props) {
@@ -13,6 +14,7 @@ class Budgeter extends React.Component{
         {name: 'second', amount: '200.942', id: 1},
       ],
       savings: '200',
+      showIO: 0,
     }
   }
 
@@ -34,6 +36,12 @@ class Budgeter extends React.Component{
     })
   }
 
+  handleIO = (visibility) => {
+    this.setState({
+      showIO: visibility,
+    })
+  }
+
   render() {
     return (
       <div className="main">
@@ -48,6 +56,11 @@ class Budgeter extends React.Component{
         paycheck={this.state.paycheck} 
         fixedExpenses={this.state.fixedExpenses} onFixedChange={this.handleFixedChange}
         savingsAmount={this.state.savings} onSavingsChange={this.handleSavingsChange} />
+
+        <Hideable name="Import & Export" 
+        visibility={this.state.showIO} onVisibilityChange={this.handleIO}>
+          <p>HELLO WORLD :)</p>
+        </Hideable>
       </div>
     );
   }
